6a666edc150d1bdbacfccb142351dce17a317542
Billy Kore <billyimmcul2010@gmail.com>
2022-11-14 15:39:55 +0700

    NewLinkedList constructor and some helper methode

--- linked_list.go
+++ linked_list.go
@@ -4,41 +4,46 @@ import (
 	"fmt"
 )
 
-type Node[T comparable] struct {
-	Value T
-	next  *Node[T]
+type node[T comparable] struct {
+	value T
+	next  *node[T]
 }
 
 type LinkedList[T comparable] struct {
-	Head *Node[T]
-	len  int
+	Head   *node[T]
+	length int
+}
+
+func NewLinkedList[T comparable](head T, values ...T) *LinkedList[T] {
+	linkedList := &LinkedList[T]{
+		Head: &node[T]{value: head},
+	}
+	for _, value := range values {
+		linkedList.append(value)
+	}
+	return linkedList
 }
 
 func (ll *LinkedList[T]) Print() {
 	current := ll.Head
 	for current != nil {
 		if current.next != nil {
-			fmt.Print(current.Value, " -> ")
+			fmt.Print(current.value, " -> ")
 		} else {
-			fmt.Println(current.Value)
+			fmt.Println(current.value)
 		}
 		current = current.next
 	}
 }
 
 func (ll *LinkedList[T]) Len() int {
-	current := ll.Head
-	for current != nil {
-		ll.len++
-		current = current.next
-	}
-	return ll.len
+	return ll.len()
 }
 
 func (ll *LinkedList[T]) Find(target T) bool {
 	current := ll.Head
 	for current != nil {
-		if current.Value == target {
+		if current.value == target {
 			return true
 		}
 		current = current.next
@@ -49,8 +54,8 @@ func (ll *LinkedList[T]) Find(target T) bool {
 func (ll *LinkedList[T]) Value(index int) T {
 	current := ll.Head
 	for current != nil {
-		if ll.len == index {
-			return current.Value
+		if ll.length == index {
+			return current.value
 		}
 		current = current.next
 	}
@@ -59,7 +64,7 @@ func (ll *LinkedList[T]) Value(index int) T {
 
 func (ll *LinkedList[T]) Reverse() {
 	current := ll.Head
-	ll.Head = new(Node[T])
+	ll.Head = new(node[T])
 	for current != nil {
 		next := current.next
 		current.next = ll.Head
@@ -69,34 +74,27 @@ func (ll *LinkedList[T]) Reverse() {
 }
 
 func (ll *LinkedList[T]) Push(data T) {
-	head := &Node[T]{Value: data}
-	head.next = ll.Head
-	ll.Head = head
+	ll.push(data)
 }
 
 func (ll *LinkedList[T]) Append(data T) {
-	newNode := &Node[T]{Value: data}
-	tail := ll.Head
-	for tail.next != nil {
-		tail = tail.next
-	}
-	tail.next = newNode
+	ll.append(data)
 }
 
 func (ll *LinkedList[T]) Insert(data T, index int) {
-	if index > ll.Len() {
+	if index > ll.len() {
 		fmt.Println("index out of list range")
 		return
 	}
 	if index == 0 {
-		ll.Push(data)
+		ll.push(data)
 		return
 	}
 	current := ll.Head
 	count := 1
 	for current != nil {
 		if count == index {
-			newNode := &Node[T]{Value: data}
+			newNode := &node[T]{value: data}
 			newNode.next = current.next
 			current.next = newNode
 			return
@@ -107,7 +105,7 @@ func (ll *LinkedList[T]) Insert(data T, index int) {
 }
 
 func (ll *LinkedList[T]) Pull() {
-	ll.Head = ll.Head.next
+	ll.pull()
 }
 
 func (ll *LinkedList[T]) Pop() {
@@ -122,12 +120,12 @@ func (ll *LinkedList[T]) Pop() {
 }
 
 func (ll *LinkedList[T]) Delete(index int) {
-	if index > ll.Len() {
+	if index > ll.len() {
 		fmt.Println("index out of list range")
 		return
 	}
 	if index == 0 {
-		ll.Pull()
+		ll.pull()
 	}
 	current := ll.Head
 	count := 1
@@ -139,4 +137,32 @@ func (ll *LinkedList[T]) Delete(index int) {
 		current = current.next
 		count++
 	}
+}
+
+func (ll *LinkedList[T]) len() int {
+	current := ll.Head
+	for current != nil {
+		ll.length++
+		current = current.next
+	}
+	return ll.length
+}
+
+func (ll *LinkedList[T]) push(data T) {
+	head := &node[T]{value: data}
+	head.next = ll.Head
+	ll.Head = head
+}
+
+func (ll *LinkedList[T]) append(data T) {
+	newNode := &node[T]{value: data}
+	tail := ll.Head
+	for tail.next != nil {
+		tail = tail.next
+	}
+	tail.next = newNode
+}
+
+func (ll *LinkedList[T]) pull() {
+	ll.Head = ll.Head.next
 }
